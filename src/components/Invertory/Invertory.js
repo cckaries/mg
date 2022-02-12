6485a96afdf337aeff5e368ab038bb5b78dbc39e
Shou-Chi Chang <cckaries@gmail.com>
2022-02-12 11:59:22 +0800

    feat(inventory): implement inventory page

--- src/components/Invertory/Invertory.js
+++ src/components/Invertory/Invertory.js
@@ -2,16 +2,24 @@ import { useEffect, useReducer } from 'react';
 import cx from 'classnames';
 
 import styles from './Invertory.module.scss';
+import Search from '../shared/Search/Search';
+import Table from '../shared/Table/Table';
 
 const Invertory = props => {
-  const [{ isPageReady }, setState] = useReducer(
+  const [{ isPageReady, searchText }, setState] = useReducer(
     (prevState, nextState) => ({ ...prevState, ...nextState }),
-    { isPageReady: false }
+    { isPageReady: false, searchText: '' }
   );
 
   return (
     <div id="container" className={styles.Container}>
-      Invertory Manager
+      <h1>Invertory Manager</h1>
+      <div className={styles.Search}>
+        <Search placeholder="Search for titles in inventory" />
+      </div>
+      <div className={styles.Table}>
+        <Table />
+      </div>
     </div>
   );
 };
