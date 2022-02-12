16cb121d360b4c09fe82c2a6a4a2723d05b5ead3
Shou-Chi Chang <cckaries@gmail.com>
2022-02-12 12:02:03 +0800

    feat(table): implement table

--- src/components/shared/Table/Table.js
+++ src/components/shared/Table/Table.js
@@ -1,7 +1,59 @@
 import styles from './Table.module.scss';
 
-const Table = () => {
-  return <table>table</table>;
+const Table = props => {
+  return (
+    <table className={styles.Container}>
+      <thead>
+        <tr>
+          <th></th>
+          <th>ID</th>
+          <th>Title Name</th>
+          <th>Season</th>
+          <th>Episode</th>
+          <th>Published</th>
+          <th>Programmable</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td>ID</td>
+          <td>Title Name</td>
+          <td>Season</td>
+          <td>Episode</td>
+          <td>Published</td>
+          <td>Programmable</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>ID</td>
+          <td>Title Name</td>
+          <td>Season</td>
+          <td>Episode</td>
+          <td>Published</td>
+          <td>Programmable</td>
+        </tr>
+        <tr className={styles.season}>
+          <td></td>
+          <td>ID</td>
+          <td>Title Name</td>
+          <td>Season</td>
+          <td>Episode</td>
+          <td>Published</td>
+          <td>Programmable</td>
+        </tr>
+        <tr className={styles.episode}>
+          <td></td>
+          <td>ID</td>
+          <td>Title Name</td>
+          <td>Season</td>
+          <td>Episode</td>
+          <td>Published</td>
+          <td>Programmable</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
