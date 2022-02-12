9bd89d959e820e9cc9b4b8764434842a8bd5f629
Shou-Chi Chang <cckaries@gmail.com>
2022-02-12 19:58:07 +0800

    feat(inventory): implement search

--- src/components/Invertory/Invertory.js
+++ src/components/Invertory/Invertory.js
@@ -1,24 +1,61 @@
 import { useEffect, useReducer } from 'react';
-import cx from 'classnames';
 
 import styles from './Invertory.module.scss';
 import InventoryTable from '../shared/InventoryTable/InventoryTable';
 import Search from '../shared/Search/Search';
 
-const Invertory = props => {
-  const [{ isPageReady, searchText }, setState] = useReducer(
+let searchTimer;
+
+const Invertory = ({
+  titles = [],
+  isTitlesReady = false,
+  onGetTitles = () => {},
+}) => {
+  const [{ searchText, processedTitles }, setState] = useReducer(
     (prevState, nextState) => ({ ...prevState, ...nextState }),
-    { isPageReady: false, searchText: '' }
+    {
+      searchText: '',
+      processedTitles: [],
+    }
   );
+
+  useEffect(() => {
+    onGetTitles();
+  }, []);
+
+  useEffect(() => {
+    if (!!searchTimer) clearTimeout(searchTimer);
+    if (!searchText.trim().length) return setState({ processedTitles: [] });
+    searchTimer = setTimeout(() => {
+      const specialCharsRegex = /[~`!@#$%^&*()+={}\[\];:\'\"<>.,\/\\\?-_\s]/g;
+      const nextProcessedTitles = titles.filter(title =>
+        title.title_name
+          ?.toLowerCase()
+          .replace(specialCharsRegex, '')
+          .includes(searchText.toLowerCase().replace(specialCharsRegex, ''))
+      );
+
+      setState({ processedTitles: nextProcessedTitles });
+    }, 300); // throttling
+  }, [searchText]);
 
   return (
     <div id="container" className={styles.Container}>
       <h1>Invertory Manager</h1>
       <div className={styles.Search}>
-        <Search placeholder="Search for titles in inventory" />
+        <Search
+          placeholder="Search for titles in inventory"
+          onChange={e => setState({ searchText: e.target.value })}
+        />
       </div>
       <div className={styles.Table}>
-        <InventoryTable titles={titles} />
+        {!isTitlesReady ? (
+          <div>Loading...</div>
+        ) : (
+          <InventoryTable
+            titles={!!searchText.trim() ? processedTitles : titles}
+          />
+        )}
       </div>
     </div>
   );
