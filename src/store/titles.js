cc38517f73ca36da9a82dec835d497ae19f24002
Shou-Chi Chang <cckaries@gmail.com>
2022-02-12 19:57:47 +0800

    feat(store): implement titles redux

--- src/store/titles.js
+++ src/store/titles.js
@@ -2,18 +2,65 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const titlesSlice = createSlice({
   name: 'titles',
-  initialState: { count: 0 },
+  initialState: { titles: [], isTitlesReady: false },
   reducers: {
-    get(state) {
+    setIsTitlesReady(state, action) {
+      state.isTitlesReady = action.payload
     },
-    post(state) {
-    },
-    put(state) {
-    },
-    delete(state) {
+    setTitles(state, action) {
+      state.titles = action.payload;
     },
   },
 });
 
-export const counterActions = titlesSlice.actions;
+export const getTitlesThunk = () => {
+  return async dispatch => {
+    dispatch(titlesActions.setIsTitlesReady(false));
+
+    const getTitles = async () => {
+      const res = await fetch(
+        'https://migo-7cc35-default-rtdb.firebaseio.com/titles.json'
+      );
+
+      return res.json();
+    };
+
+    try {
+      const resJson = await getTitles();
+      dispatch(titlesActions.setTitles(resJson));
+      dispatch(titlesActions.setIsTitlesReady(true));
+    } catch (err) {
+      throw new Error('ERROR:', err);
+    }
+  };
+};
+
+// export const putTitlesThunk = payload => {
+//   return async dispatch => {
+//     const putTitles = async () => {
+//       const res = await fetch(
+//         'https://migo-7cc35-default-rtdb.firebaseio.com/titles.json',
+//         {
+//           method: 'PUT',
+//           body: JSON.stringify({
+//             titles: payload,
+//           }),
+//         }
+//       );
+
+//       if (!res.ok) throw new Error('Error: Failed to update DB');
+//     };
+
+//     try {
+//       await putTitles();
+//       const resJson = await getTitlesThunk();
+//       console.log('res:', resJson);
+//       dispatch(titlesActions.setTitles(resJson));
+//     } catch (err) {
+//       throw new Error('ERROR:', err);
+//     }
+//   };
+// };
+
+export const titlesActions = titlesSlice.actions;
 export default titlesSlice.reducer;
