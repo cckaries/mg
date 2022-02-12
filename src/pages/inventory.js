8e05d64d0561be0d9aecf5c3d27c3d7338d359dc
Shou-Chi Chang <cckaries@gmail.com>
2022-02-12 19:57:10 +0800

    feat(inventory): connect redux actions & reducer

--- src/pages/inventory.js
+++ src/pages/inventory.js
@@ -1,7 +1,19 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import Invertory from '../components/Invertory/Invertory';
+import { getTitlesThunk } from '../store/titles';
 
 const InventoryPage = () => {
-  return <Invertory />;
+  const dispatch = useDispatch();
+  const { titles, isTitlesReady } = useSelector(state => state.titles);
+
+  return (
+    <Invertory
+      titles={titles}
+      isTitlesReady={isTitlesReady}
+      onGetTitles={() => dispatch(getTitlesThunk())}
+    />
+  );
 };
 
 export default InventoryPage;
